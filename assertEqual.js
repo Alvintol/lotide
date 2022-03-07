d2a146a0598b3039071593ee6d6da569c3233496
Alvintol <alvin.tol@hotmail.com>
2022-03-07 22:12:50 +0000

    initial commit with basic assert function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🛑 Assertion Passed: [${actual}] === [${expected}] 🛑`);
+  } else {
+    console.log(`✅ Assertion Failed: [${actual}] !== [${expected}] ✅`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
