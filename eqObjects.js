8a98808f35637125d81ce2a4b720ab420f4afa71
Alvintol <alvin.tol@hotmail.com>
2022-03-19 21:39:57 +0000

    complete eqObjects with correct recursive functionality

--- eqObjects.js
+++ eqObjects.js
@@ -1,17 +1,25 @@
+const assertEqual = require('./assertEqual');
+
 const eqObjects = (object1, object2) => {
-  let result = false;
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  } else {
-    for (let keyA in object1) {
-      for (let keyB in object2) {
-        if (object1[keyA] === object2[keyB]) {
-          result = true;
-        }
-      }
-    }
+  if (typeof object1 !== 'object' && typeof object2 !== 'object'){
+    return object1 === object2;
   }
-  return result;
+  if (typeof object1 === 'object' && typeof object2 !== 'object'||
+  typeof object1 !== 'object' && typeof object2 === 'object'||
+  Object.keys(object1).length !== Object.keys(object2).length) {{
+    return false;
+  }}
+  for (const key in object1) {
+    if (!Object.keys(object2).includes(key) ||
+    !eqObjects(object1[key], object2[key])){
+      return false;
+    }
+  } 
+  return true
 };
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)
+assertEqual(eqObjects({ a: { y: 5, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false)
+assertEqual(eqObjects({ a: { y: 5, z: 1 }, b: 2 }, { a: 1, b: 2 }), false)
 
 module.exports = eqObjects;
